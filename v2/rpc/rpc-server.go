65bde4091cffa5c1bf967398f2fcdca1218d4ea1
yoeria <yoeri08@hotmail.com>
2021-12-28 12:07:09 +0100

    init v2

---
+++ v2/rpc/rpc-server.go
@@ -0,0 +1,36 @@
+// Package main implements a server for chartDataExchange service.
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func (s *server) handleChartDataRequest(ctx context.Context, in )
+
+func server() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	grpc.(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
--- v2/websocket.go
+++ v2/websocket.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 
+	"./helpers"
 	"github.com/go-numb/go-ftx/realtime"
 )
 
