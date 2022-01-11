a37077030b2be5d1e48a1b998e27b279b30d8344
yoeria <yoeri08@hotmail.com>
2022-01-11 20:37:09 +0100

    clean up code

--- rpc-server.go
+++ rpc-server.go
@@ -6,8 +6,6 @@ import (
 	"log"
 	"net"
 
-	"google.golang.org/grpc"
-
 	pb "github.com/yoeria/goftxlogistics/protos"
 )
 
@@ -15,12 +13,12 @@ const (
 	port = ":50051"
 )
 
-func (s *pb.ChartDataExchangeServer) serveWindowDataArray(ctx context.Context, in *pb.) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.GetName())
+func (s *pb.ChartDataExchangeServer) serveWindowDataArray(ctx context.Context, in *pb.ChartDataExchangeClient) (*pb.WindowDataArray, error) {
+	log.Printf("Received: %v")
 	return &pb.WindowDataArray{}, nil
 }
 
-func (s *server) handleChartDataRequest(ctx context.Context, in *pb.ChartDataRequest) (*pb.ChartDataReply, error) {
+func handleChartDataRequest(ctx context.Context) {
 }
 
 func server() {
@@ -28,10 +26,8 @@ func server() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
 
-	grpc.
-		log.Printf("server listening at %v", lis.Addr())
+	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
