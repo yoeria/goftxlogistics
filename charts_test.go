31200d12ccf895a9415991a44cc0fca46110ac00
yoeria <yoeri08@hotmail.com>
2022-02-26 17:54:38 +0100

    set test template KlineChart

--- charts_test.go
+++ charts_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
@@ -22,4 +23,23 @@ func TestChart(t *testing.T) {
 	//spew.Dump(t)
 	color.Reset()
 	serveChart()
+}
+
+func Test_KlineChart(t *testing.T) {
+	type args struct {
+		w   http.ResponseWriter
+		in1 *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(_ *testing.T) {
+			KlineChart(tt.args.w, tt.args.in1)
+		})
+	}
 }
