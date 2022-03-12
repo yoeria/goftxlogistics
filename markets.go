b54aa61e35dbd0b6631b972177da8779cfedc17d
yoeria <yoeri08@hotmail.com>
2022-03-11 23:40:04 -0800

    work on slices containing info for potential futur

--- markets.go
+++ markets.go
@@ -2,24 +2,63 @@ package main
 
 import (
 	"log"
+	"sort"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/go-numb/go-ftx/rest/public/futures"
+	"github.com/schigh/slice"
 )
 
 var (
 	rc = RestClient
 )
 
-func currentPotentFutures() (list []futures.FutureForList) {
+type TradableFutures struct {
+	Name                              string
+	Change1H, Change24H, VolumeUsd24H float64
+}
+
+type TradableFuturesSlice []TradableFutures
+
+func (in *TradableFuturesSlice) Sort() (ret []TradableFutures) {
+	inn := *in
+	 sort.SliceStable(*in,
+		func(i int, j int) bool { return inn[i].VolumeUsd24H < inn[j].VolumeUsd24H })
+	ret = *in
+	return
+}
+
+func CurrentPotentFutures(perpetual bool) (list []*TradableFutures) {
 	getFutures, err := rc.Futures(&futures.RequestForFutures{})
+	rangeFutures := *getFutures
+
 	if err != nil {
 		return
 	}
-	for range *getFutures {
-		list = append(list, *getFutures...)
+	ret := getFutureList(perpetual, rangeFutures, make([]futures.FutureForList, 0))
+
+	// work todo???
+	for i := range ret {
+
 	}
-	spew.Dump(getFutures)
+
+	log.Println(len(*getFutures))
 	log.Println(len(list))
+	return list
+}
+
+
+func getFutureList(perpetual bool, rangeFutures futures.ResponseForFutures, list []futures.FutureForList) []futures.FutureForList {
+	if perpetual {
+		for i := range rangeFutures {
+			if rangeFutures[i].Perpetual {
+				list = append(list, rangeFutures[i])
+			}
+
+		}
+	} else {
+		for v := range rangeFutures {
+			list = append(list, rangeFutures[v])
+		}
+	}
 	return list
 }
