e8885361b3b126c5650b786b2dde592834604940
yoeria <yoeri08@hotmail.com>
2022-03-25 12:36:18 +0100

    test for appending a struct to redis db

--- db/redis_test.go
+++ db/redis_test.go
@@ -2,7 +2,11 @@ package db
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/yoeria/goftxlogistics/structs"
 )
 
 // Test validity of redis connection.
@@ -12,4 +16,27 @@ func Test(t *testing.T) {
 
 	fmt.Printf("t: %v\n", t)
 	fmt.Println(testCommand)
+}
+
+func AppendTradeToTrades(t *testing.T) {
+	ctx := RDB.Context()
+	count := 100
+	for i := 0; i < count; i++ {
+		RDB.ZAdd(ctx, "TradesTesting", &redis.Z{
+			Score: rand.Float64(),
+			Member: structs.Trade{
+				DatetimeEnter: "2022-01-01",
+				DatetimeExit:  "2022-01-02",
+				Enter:         float64(i ^ 2),
+				Exit:          float64(i ^ 2),
+				Net:           float64(i ^ 2),
+				NetPercent:    float64(i ^ 2),
+				Wallet:        float64(i ^ 2),
+				WalletNet:     float64(i ^ 2),
+				Fees:          float64(i ^ 2),
+			},
+		})
+
+	}
+
 }
