f0d16a40c2115bd02965ad1bf85d3c9ae87f816f
yoeria <yoeri08@hotmail.com>
2022-05-19 15:31:20 +0200

    changes

--- main.go
+++ main.go
@@ -1,30 +1,51 @@
 package main
 
+import (
+	"github.com/go-numb/go-ftx/rest/private/orders"
+)
+
+type Broker struct{}
+
 type MainProcess struct {
 	MainProcessInfo      *mainProcessInfo
 	ActivePositionsCount int64
 	Action               chan Buy
 }
 
-type mainProcessInfo struct {}
-
-// Actions possible for the MainProcess to execute
-type Action chan struct {
-	Buy  *Buy
-	Sell *Sell
-}
+type mainProcessInfo struct{}
 
 // Buy instruction details
-type Buy struct {
+type Buy chan struct {
+	Currency string
 }
 
-// Buy instruction details
-type Sell struct {
+// Sell instruction details
+type Sell chan struct {
+	Currency string
 }
-
-
+type Order *orders.RequestForPlaceOrder
 func main() {
 	//login
 	LoadCreds("./")
+	Order
+	chanBuy := make(chan Buy)
+	chanSell := make(chan Sell)
+	select {
+	case b := <-chanBuy:
+		rc.PlaceOrder(&orders.RequestForPlaceOrder{
+			ClientID:          "use_original_client_id",
+			Type:              "buy",
+			Market:            ,
+			Side:              "",
+			Price:             0,
+			Size:              0,
+			ReduceOnly:        false,
+			Ioc:               false,
+			PostOnly:          false,
+			RejectOnPriceBand: false,
+		})
+	case s <- chanSell:
+		rc.PlaceOrder(Order)
+	}
 
 }
