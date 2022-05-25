42e2677f62e5beb519523d9eefd0095fe5e6d7f6
yoeria <yoeri08@hotmail.com>
2022-05-25 17:02:52 +0200

    change structure of pubsub

--- pubsub.go
+++ pubsub.go
@@ -6,32 +6,34 @@ import (
 	"time"
 )
 
+
 // Use this subscription to subscribe to messages related to the "bot" preferences
-func pubsub() {
-
-	pubsub := rdb.Subscribe(ctx, "mychannel1")
-
 	// Wait for confirmation that subscription is created before publishing anything.
+	// Go channel which receives messages.
+	// Publish a message.
+	// When pubsub is closed channel is closed too.
+	// Consume messages.
+func SubToPreferences() {
+	pubsub := rdb.Subscribe(ctx, "preferences")
+
 	_, err := pubsub.Receive(ctx)
 	if err != nil {
 		panic(err)
 	}
 
-	// Go channel which receives messages.
 	ch := pubsub.Channel()
 
-	// Publish a message.
-	err = rdb.Publish(ctx, "mychannel1", "hello").Err()
+	// In preferences interface:
+	err = rdb.Publish(ctx, "preferences", "hello").Err()
 	if err != nil {
 		panic(err)
 	}
 
 	time.AfterFunc(time.Second, func() {
-		// When pubsub is closed channel is closed too.
+
 		_ = pubsub.Close()
 	})
 
-	// Consume messages.
 	for msg := range ch {
 		fmt.Println(msg.Channel, msg.Payload)
 	}
