ae285144b4a63e33dda13a86dbe78bd320c2e914
yoeria <yoeri08@hotmail.com>
2022-06-08 20:44:34 +0200

    [MAJOR] create first instance"p0"

--- main.go
+++ main.go
@@ -4,15 +4,36 @@ import (
 	"fmt"
 )
 
+// Set default values in init func
+func init() {
+	// Fill in rdb suppliable struct vars
+	UpdateConfiguration()
+}
+
+// Launch instances by creating a variable with the value of struct ProcessInformation
 func main() {
 	//login
 	LoadCreds("./")
 
+	// Make instance of ProcessInformation
+	p0 := &ProcessInformation{
+		Statistics: &Statistics{
+			ActivePositionsCount: 0,
+		},
+		Preferences: aPreferences,
+
+		SyncInfo: aSyncInfo,
+	}
+
 	// Make channel that receives all Order requests
 	chanOrder := make(chan Order)
 	select {
-	case o := <-chanOrder:
-		fmt.Println(o.Side)
+	case order := <-chanOrder:
+		if aPreferences.MaxConcurrentPositions > p0.Statistics.ActivePositionsCount {
+			order.Exec()
+			// TODO: Place check routine if order is filled or not
+		}
+		fmt.Println(order.Side)
 		break
 	// Default case is check to sync config with server
 	default:
@@ -22,14 +43,8 @@ func main() {
 
 }
 
-// Set default values in init func
-func init() {
-	// Fill in rdb suppliable struct vars
-	UpdateConfiguration()
-}
-
 // This function triggers the update/initialization of the configuration vars
 func UpdateConfiguration() {
-	aPreferences.Update()
-	aStrategies.Update()
+	go aPreferences.Update()
+	go aStrategies.Update()
 }
