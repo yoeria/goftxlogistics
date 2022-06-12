fb431dd87e7ca9d65ceb824a2e05a74f827ed689
yoeria <yoeri08@hotmail.com>
2022-06-12 14:54:21 +0200

    reset test

--- markets_test.go
+++ markets_test.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
-func Test_CurrentPotentFutures(t *testing.T) {
-	CurrentPotentFutures(true)
+func TestCurrentPotentFutures(t *testing.T) {
+	type args struct {
+		perpetual bool
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wantList []*TradableFutures
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotList := CurrentPotentFutures(tt.args.perpetual); !reflect.DeepEqual(gotList, tt.wantList) {
+				t.Errorf("CurrentPotentFutures() = %v, want %v", gotList, tt.wantList)
+			}
+		})
+	}
 }
